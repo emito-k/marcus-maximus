25901e85342ad51532ef1c61db6cc7374d874620
Emito Khoza <emito.khoza@gmail.com>
2024-05-17 23:25:11 +0200

    Integrated the interface into the code

--- frontend/src/app/shared/components/view-school-card/view-school-card.component.ts
+++ frontend/src/app/shared/components/view-school-card/view-school-card.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { ISchool } from '../../models/school.interface';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-view-school-card',
   standalone: true,
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule
   ],
   templateUrl: './view-school-card.component.html',
   styleUrl: './view-school-card.component.css'
 })
 export class ViewSchoolCardComponent {
-
+  @Input() school: ISchool = {
+    school_name: 'School Name',
+    school_id: 'School ID',
+    description: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+    A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+    bred for hunting.`,
+    thumbnail_url: 'https://via.placeholder.com/750x600',
+    profile_url: 'https://via.placeholder.com/509x500'
+  };
 }
