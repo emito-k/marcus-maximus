333aabcfd95c439fabba1dfee7489ea395658ac2
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 00:21:39 +0200

    Added tabs

--- frontend/src/app/view-school-page/view-school-page.component.ts
+++ frontend/src/app/view-school-page/view-school-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-view-school-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatTabsModule
+  ],
   templateUrl: './view-school-page.component.html',
   styleUrl: './view-school-page.component.css'
 })
