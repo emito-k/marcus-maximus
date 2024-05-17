3bf7cb6a6408999011cdedf6d5339e6f6bef7638
Emito Khoza <emito.khoza@gmail.com>
2024-05-17 12:47:52 +0200

    Added search bar in home page

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    // MatButtonModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
