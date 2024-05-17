e8de6aedfa25f636a81f1a2cdba2369e9affa964
Emito Khoza <emito.khoza@gmail.com>
2024-05-17 22:53:26 +0200

    Fixed layout for cards

--- frontend/src/app/shared/components/view-schools/view-schools.component.ts
+++ frontend/src/app/shared/components/view-schools/view-schools.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 // import { MatCardModule } from '@angular/material/card';
 import { ViewSchoolCardComponent } from '../view-school-card/view-school-card.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 @Component({
   selector: 'app-view-schools',
   standalone: true,
   imports: [
-    ViewSchoolCardComponent
+    ViewSchoolCardComponent,
+    FlexLayoutModule
   ],
   templateUrl: './view-schools.component.html',
   styleUrl: './view-schools.component.css'
