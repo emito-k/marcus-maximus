78c953fdf1a504e817112b1464c9a5bad6d4aa84
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 12:01:14 +0200

    Added school members list

---
+++ frontend/src/app/school-settings-page/forms/school-members-list/school-members-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-members-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './school-members-list.component.html',
+  styleUrl: './school-members-list.component.css'
+})
+export class SchoolMembersListComponent {
+
+}
