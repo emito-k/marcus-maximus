347250db373c876e6c8b445864f233050158eb86
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:08:50 +0200

    Fixed school module cards layout

--- frontend/src/app/shared/components/school-modules-feed/school-modules-feed.component.ts
+++ frontend/src/app/shared/components/school-modules-feed/school-modules-feed.component.ts
@@ -2,13 +2,15 @@ import { Component, Input } from '@angular/core';
 import { ISchoolModule } from '../../models/school-module.interface';
 import { SchoolModuleCardComponent } from '../school-module-card/school-module-card.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-school-modules-feed',
   standalone: true,
   imports: [
     SchoolModuleCardComponent,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    FlexLayoutModule
   ],
   templateUrl: './school-modules-feed.component.html',
   styleUrl: './school-modules-feed.component.css'
