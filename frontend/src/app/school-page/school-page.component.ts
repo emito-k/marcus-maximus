2680f65e8007c7910fc2effe7a13fcfa40621589
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:07:12 +0200

    Added school module cards

--- frontend/src/app/school-page/school-page.component.ts
+++ frontend/src/app/school-page/school-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import { SchoolModulesFeedComponent } from '../shared/components/school-modules-feed/school-modules-feed.component';
+import { SchoolService } from '../shared/services/school.service';
+import { ActivatedRoute } from '@angular/router';
+import { ISchoolModule } from '../shared/models/school-module.interface';
 @Component({
   selector: 'app-school-page',
   standalone: true,
-  imports: [],
+  imports: [
+    SchoolModulesFeedComponent,
+  ],
   templateUrl: './school-page.component.html',
   styleUrl: './school-page.component.css'
 })
 export class SchoolPageComponent {
+  modules: ISchoolModule[] = [];
+  loading: boolean = true;
 
+  constructor(private schoolService: SchoolService, private route: ActivatedRoute) {
+    const school_id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.getModules(school_id);
+  }
+
+  async getModules(school_id: string): Promise<void> {
+    this.loading = true;
+    this.modules = await this.schoolService.getSchoolModules(school_id);
+    this.loading = false;
+  }
 }
--- frontend/src/app/shared/components/school-modules-feed/school-modules-feed.component.ts
+++ frontend/src/app/shared/components/school-modules-feed/school-modules-feed.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { ISchoolModule } from '../../models/school-module.interface';
 import { SchoolModuleCardComponent } from '../school-module-card/school-module-card.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-school-modules-feed',
   standalone: true,
   imports: [
-    SchoolModuleCardComponent
+    SchoolModuleCardComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './school-modules-feed.component.html',
   styleUrl: './school-modules-feed.component.css'
 })
 export class SchoolModulesFeedComponent {
+  @Input() loading: boolean = true;
   @Input() modules: ISchoolModule[] = [];
 }
