2144ea68a6f4e78a0a79da7ad1ad5143ea9e4247
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:42:53 +0200

    Added school information form component

---
+++ frontend/src/app/school-settings-page/forms/school-information/school-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './school-information.component.html',
+  styleUrl: './school-information.component.css'
+})
+export class SchoolInformationComponent {
+
+}
