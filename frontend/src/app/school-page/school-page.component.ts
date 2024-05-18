cb679438a4f31158ea27857488c940d9b7937d9b
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:14:33 +0200

    Added search for modules

--- frontend/src/app/school-page/school-page.component.ts
+++ frontend/src/app/school-page/school-page.component.ts
@@ -3,11 +3,18 @@ import { SchoolModulesFeedComponent } from '../shared/components/school-modules-
 import { SchoolService } from '../shared/services/school.service';
 import { ActivatedRoute } from '@angular/router';
 import { ISchoolModule } from '../shared/models/school-module.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-school-page',
   standalone: true,
   imports: [
     SchoolModulesFeedComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   templateUrl: './school-page.component.html',
   styleUrl: './school-page.component.css'
@@ -15,6 +22,7 @@ import { ISchoolModule } from '../shared/models/school-module.interface';
 export class SchoolPageComponent {
   modules: ISchoolModule[] = [];
   loading: boolean = true;
+  searching: boolean = false;
 
   constructor(private schoolService: SchoolService, private route: ActivatedRoute) {
     const school_id = this.route.snapshot.paramMap.get('id') ?? '';
@@ -25,5 +33,17 @@ export class SchoolPageComponent {
     this.loading = true;
     this.modules = await this.schoolService.getSchoolModules(school_id);
     this.loading = false;
+  }
+
+  async searchForModules(query: string): Promise<void> {
+    this.searching = true;
+    // this.modules = await this.schoolService.searchSchoolModules(query);
+    this.searching = false;
+  }
+
+  async clearSearch(): Promise<void> {
+    this.searching = false;
+    const school_id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.getModules(school_id);
   }
 }
