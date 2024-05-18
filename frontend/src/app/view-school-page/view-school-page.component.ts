a8339de393bf477fe88486fba49d34e06f4a8893
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 08:07:24 +0200

    Added dummy calls for school, modules and members in view school page

--- frontend/src/app/view-school-page/view-school-page.component.ts
+++ frontend/src/app/view-school-page/view-school-page.component.ts
@@ -3,6 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { SchoolService } from '../shared/services/school.service';
+import { ActivatedRoute } from '@angular/router';
+import { ISchool } from '../shared/models/school.interface';
+import { ISchoolModule } from '../shared/models/school-module.interface';
+import { ISchoolMember } from '../shared/models/school-member.interface';
 
 @Component({
   selector: 'app-view-school-page',
@@ -17,5 +22,26 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrl: './view-school-page.component.css'
 })
 export class ViewSchoolPageComponent {
+  school: ISchool | null = null;
+  modules: ISchoolModule[] = [];
+  members: ISchoolMember[] = [];
 
+  constructor(private schoolService: SchoolService, private router: ActivatedRoute) {
+    const school_id = this.router.snapshot.paramMap.get('id') ?? '';
+    this.getSchool(school_id);
+    this.getSchoolModules(school_id);
+    this.getSchoolMembers(school_id);
+  }
+
+  async getSchool(school_id: string) {
+    this.school = await this.schoolService.getSchoolDetail(school_id);
+  }
+
+  async getSchoolModules(school_id: string) {
+    this.modules = await this.schoolService.getSchoolModules(school_id);
+  }
+
+  async getSchoolMembers(school_id: string) {
+    this.members = await this.schoolService.getSchoolMembers(school_id);
+  }
 }
