29a8831bbc8dec9c7c2e4f21b389bd9df16fa7d0
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:59:30 +0200

    Updated the school information form

--- frontend/src/app/school-settings-page/forms/school-information/school-information.component.ts
+++ frontend/src/app/school-settings-page/forms/school-information/school-information.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ISchool } from '../../../shared/models/school.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-school-information',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './school-information.component.html',
   styleUrl: './school-information.component.css'
 })
 export class SchoolInformationComponent {
+  @Input({ required: true }) school_id!: string;
+  school: ISchool | null = null;
 
+  constructor() { }
 }
--- frontend/src/app/school-settings-page/school-settings-page.component.ts
+++ frontend/src/app/school-settings-page/school-settings-page.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-
+import { ActivatedRoute } from '@angular/router';
+import { SchoolInformationComponent } from './forms/school-information/school-information.component';
 @Component({
   selector: 'app-school-settings-page',
   standalone: true,
   imports: [
-    MatTabsModule
+    MatTabsModule,
+    SchoolInformationComponent
   ],
   templateUrl: './school-settings-page.component.html',
   styleUrl: './school-settings-page.component.css'
 })
 export class SchoolSettingsPageComponent {
+  school_id : string = '';
 
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.school_id = params['school_id'];
+    });
+  }
 }
