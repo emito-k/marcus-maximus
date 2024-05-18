fc4bcb21c7c2a386ba9354bb33ac0b1beee0ac96
Emito Khoza <emito.khoza@gmail.com>
2024-05-18 11:31:34 +0200

    Added tabs to settings page

--- frontend/src/app/school-settings-page/school-settings-page.component.ts
+++ frontend/src/app/school-settings-page/school-settings-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-school-settings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule
+  ],
   templateUrl: './school-settings-page.component.html',
   styleUrl: './school-settings-page.component.css'
 })
