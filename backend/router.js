a4c20a2ebd4c40025032bbbc67653565bcb232a2
Emito Khoza <emito.khoza@gmail.com>
2024-05-25 22:45:48 +0200

    Added the user create feature

--- backend/router.js
+++ backend/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { createUser } = require("./services");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
+router.get("/helloworld", (req, res) => {
+  res.send("Hello world!");
 });
+
+router.post("/users", createUser);
 
 module.exports = router;
---
+++ backend/services/create-user.js
@@ -0,0 +1,24 @@
+const { User } = require("../data-access/sequalize");
+
+async function createUser(req, res) {
+  try {
+    const { username, password, email, first_name, last_name } = req.body;
+
+    const user = await User.create({
+      username,
+      password,
+      email,
+      first_name,
+      last_name,
+      date_created: new Date(),
+      last_login: new Date(),
+    });
+
+    res.status(201).send(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while creating the user.");
+  }
+}
+
+module.exports = createUser;
