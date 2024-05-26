05ec13ecf986584fec44188c3161b263e37889c1
Emito Khoza <emito.khoza@gmail.com>
2024-05-26 09:41:28 +0200

    Made the search more extensive

--- backend/services/role/get-roles.js
+++ backend/services/role/get-roles.js
@@ -1,14 +1,31 @@
 const { Role } = require("../../data-access/sequalize");
+const { Op } = require("sequelize");
 
 async function getRoles(req, res) {
   try {
     // make use of filters
     const filters = {};
 
-    const { school_id } = req.query;
+    const { school_id, id, name, description } = req.query;
 
     if (school_id) {
       filters.school_id_fk = school_id;
+    }
+
+    if (id) {
+      filters.id = id;
+    }
+
+    if (name) {
+      filters.name = {
+        [Op.like]: `%${name}%`,
+      };
+    }
+
+    if (description) {
+      filters.description = {
+        [Op.like]: `%${description}%`,
+      };
     }
 
     const roles = await Role.findAll({
